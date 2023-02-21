24fb17f852dea9ca0b9b91660d03a91ab5779d23
Rian013 <rianarchfernandes013@gmail.com>
2023-02-21 10:07:20 +0530

    done

---
+++ Regular-Assignment/Week-3/Day-11/AllInteger.java
@@ -0,0 +1,11 @@
+class AllInteger{
+  public static void main(String args[]){
+    int i =1;
+      while(i<=100){
+      if(i%5==0)
+      System.out.println(i);
+      i=i+1;
+        }
+    System.out.println("exit");
+  }
+}
